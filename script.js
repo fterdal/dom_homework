3cc74b86ebbac6b1a33fab3f0e8a352a57739db7
Finn <finnterdal@gmail.com>
2019-07-08 15:55:02 -0500

    finishes slice 1

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
  *   SLICE 1
  **************/
 
-function updateCoffeeView(coffeeQty) {}
+function updateCoffeeView(coffeeQty) {
+  const coffeeCounter = document.getElementById('coffee_counter')
+  coffeeCounter.innerText = coffeeQty
+}
 
-function clickCoffee(data) {}
+function clickCoffee(data) {
+  updateCoffeeView(++data.coffee)
+}
 
 /**************
  *   SLICE 2
@@ -49,21 +54,21 @@ function tick(data) {}
 if (typeof process === 'undefined') {
   // Get starting data from the window object
   // (This comes from data.js)
-  const data = window.data;
+  const data = window.data
 
   // Add an event listener to the giant coffee emoji
-  const bigCoffee = document.getElementById('big_coffee');
-  bigCoffee.addEventListener('click', () => clickCoffee(data));
+  const bigCoffee = document.getElementById('big_coffee')
+  bigCoffee.addEventListener('click', () => clickCoffee(data))
 
   // Add an event listener to the container that holds all of the producers
   // Pass in the browser event and our data object to the event listener
-  const producerContainer = document.getElementById('producer_container');
+  const producerContainer = document.getElementById('producer_container')
   producerContainer.addEventListener('click', event => {
-    buyButtonClick(event, data);
-  });
+    buyButtonClick(event, data)
+  })
 
   // Call the tick function passing in the data object once per second
-  setInterval(() => tick(data), 1000);
+  setInterval(() => tick(data), 1000)
 } else if (process) {
   // This exports the code written here so we can import and test it in Mocha
   module.exports = {
@@ -80,6 +85,6 @@ if (typeof process === 'undefined') {
     updatePrice,
     attemptToBuyProducer,
     buyButtonClick,
-    tick
-  };
+    tick,
+  }
 }
